96bf8418783ceb4a04d3a7835f92945038e7de58
angelojohenry <johenry.angelo@gmail.com>
2025-10-01 08:48:28 +0700

    Tugas 5 selesai

--- main/urls.py
+++ main/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from main.views import show_main, add_object, show_object, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user
+from main.views import show_main, add_object, show_object, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user, edit_product, delete_product
 
 app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
     path('add-object/', add_object, name='add_object'),
-    path('item/<str:id>/', show_object, name='show_object'),
+    path('product/<str:id>/', show_object, name='show_object'),
     path('xml/', show_xml, name= 'show_xml'),
     path('json/', show_json, name='show_json'),
     path('xml/<str:store_id>/', show_xml_by_id, name='show_xml_by_id'),
@@ -14,4 +14,6 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
+    path('product/<uuid:id>/edit', edit_product, name='edit_product'),
+    path('product/<uuid:id>/delete', delete_product, name='delete_product'),
 ]
